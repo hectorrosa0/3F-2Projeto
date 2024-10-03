7828468cc39ccc9bc3852143fc31678769400b13
hectorrosa0 <127422444+hectorrosa0@users.noreply.github.com>
2024-10-03 09:46:19 -0300

    03/10/2024

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -2,12 +2,18 @@ const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-g
 
 async function visualizaDadosGlobais(){
     const res = await fetch(url);
+    console.log(res);
     const dados = await res.json();
+    const totalPessoasMundo = (dados.total_pessoas_mundo)/1e9;
+    const totalPessoasConectadas = (dados.total_pessoas_conectadas)/1e9;
+    const tempoMedio = dados.tempo_medio;
     console.log(dados);
 
  const paragrafo = document.createElement('p')
  paragrafo.classList.add('graficos-texto')
  const container = document.getElementById('graficos-container')
+ paragrafo.innerHTML = `No mundo tem ${totalPessoasMundo} bilhoes de habitantes,dos quais ${totalPessoasConectadas}bilhoes estao em alguma rede social por um tempo medio de ${tempoMedio}`
+container.appendChild(paragrafo);
 }
 
 visualizaDadosGlobais();
