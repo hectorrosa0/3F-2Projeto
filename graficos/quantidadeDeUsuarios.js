40c799c23d02b6fc37742e635b930a8df665b41c
hectorrosa0 <127422444+hectorrosa0@users.noreply.github.com>
2024-10-11 08:54:36 -0300

    11/10/2024

--- graficos/quantidadeDeUsuarios.js
+++ graficos/quantidadeDeUsuarios.js
@@ -2,8 +2,8 @@
 async function quantidadeDeUsuarios(){
 
     const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
-    const dados = await res.json();
     const res = await fetch(url)
+    const dados = await res.json();
     const nomeDasRedes = Object.keys(dados);
     const  quantidadeDeUsuarios = Object.values(dados);
 
@@ -14,4 +14,11 @@ async function quantidadeDeUsuarios(){
     type: 'bar'
 }
     ]
+
+const grafico = document.createElement('div');
+grafico.className = 'grafico';
+document.getElementById('graficos-container').appendChild(grafico);
+Plotly.newPlot(grafico,infos);
 }
+
+quantidadeDeUsuarios();
